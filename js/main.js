83605428c9780af1edcdf223e6dd37f848467bec
Itilon <ivanov.elect@gmail.com>
2021-01-24 00:02:53 +0200

    Project page update

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const sidenavs = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenavs, {});
 
+    sliders = document.querySelectorAll('.slider');
+    M.Slider.init(sliders, {});
 
     const images = document.querySelectorAll('.materialboxed');
     M.Materialbox.init(images, {});
