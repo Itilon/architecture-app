9e303a0b0a7a4300083000bac2c238afeb90e538
Itilon <ivanov.elect@gmail.com>
2021-01-23 16:18:09 +0200

    Sidenav and contact page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const sidenavs = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenavs, {});
+
+
     const images = document.querySelectorAll('.materialboxed');
     M.Materialbox.init(images, {});
 });
