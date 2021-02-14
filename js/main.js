4bdbfae951cc7045694c8e8b53ab38bd63ae49df
Itilon <ivanov.elect@gmail.com>
2021-02-14 22:06:33 +0200

    Contact form styling and function code

--- js/main.js
+++ js/main.js
@@ -12,6 +12,12 @@ document.addEventListener('DOMContentLoaded', () => {
         onOpenStart: onOpenStart.bind(null, sliders[0]),
         onCloseEnd: onCloseEnd.bind(null, sliders[0])
     });
+
+    const contactForm = document.querySelector('.contact-form form');
+    
+    if (contactForm) {
+        contactForm.addEventListener('submit', submitContactForm.bind(null, contactForm));
+    }
 });
 
 const controlNavbarVisibility = () => {
@@ -41,4 +47,70 @@ const onCloseEnd = (slider) => {
         const sliderInstance = M.Slider.getInstance(slider);
         sliderInstance.start();
     }
+};
+
+const submitContactForm = (form) => {
+    this.event.preventDefault();
+
+    sendFormData(form)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error();
+            }
+
+            [...form.elements].forEach((element) => {
+                element.value = '';
+            });
+
+            populateFormMessage(form, true);
+        })
+        .catch(() => {
+            populateFormMessage(form, false);
+        });
+};
+
+const sendFormData = (form) => {
+    const endpoint = form.action;
+    const method = form.method;
+    const formData = new FormData(form);
+    
+    return fetch(
+        endpoint,
+        {
+            method,
+            body: formData 
+        }
+    );
+};
+
+const populateFormMessage = (form, isSuccess) => {
+    const messageContainer = isSuccess ?
+        createElement('div', 'success-message', null) :
+        createElement('div', 'error-message', null);
+
+    const message = isSuccess ?
+        createElement('p', null, 'Съобщението е изпратено успешно.') :
+        createElement('p', null, 'Нещо се обърка. Моля, опитай отново.');
+
+    const exitBtn = createElement('span', 'exit-btn', '&#10005');
+
+    messageContainer.appendChild(message);
+    messageContainer.appendChild(exitBtn);
+
+    form.prepend(messageContainer);
+
+    exitBtn.addEventListener('click', () => removeElement(messageContainer));
+
+    setTimeout(() => removeElement(messageContainer), 5000);
+};
+
+const createElement = (tagName, className, innerText) => {
+    const element = document.createElement(tagName);
+    element.classList.add(className);
+    element.innerHTML = innerText;
+    return element;
+};
+
+const removeElement = (element) => {
+    element.remove();
 };
